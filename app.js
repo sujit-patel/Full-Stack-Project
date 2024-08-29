37a17048b90c5d2a4b15483041065927b49660ce
sujit patel <sujitworkclg@gmail.com>
2024-08-29 22:50:04 +0530

    Add more features

--- app.js
+++ app.js
@@ -38,28 +38,10 @@ app.post('/create', async (req, res) => {
     }
 });
 
+// show users 
 app.get('/userdata', async (req, res) => {
     let alldata = await userModel.find();
     res.render("userdata", {udata: alldata});
-});
-
-app.get('/delete', async (req, res) => {
-    res.render("delete");
-});
-
-app.post('/delete', async (req, res) => {
-    const { name } = req.body;
-    try {
-        let deleteuser = await userModel.findOneAndDelete({ name: name });
-        if (deleteuser) {
-            res.redirect(`/delete?message=User ${name} deleted successfully!`);
-        } else {
-            res.redirect(`/delete?message=User ${name} not found.`);
-        }
-    } catch (err) {
-        console.error(err);
-        res.redirect(`/delete?message=Error deleting user`);
-    }
 });
 
 // profile Route
@@ -67,6 +49,46 @@ app.get('/profile/:name', async (req, res) => {
     let userProfile = await userModel.findOne({name: req.params.name});
     res.render("profile", {userProfile});
 });
+
+app.post('/update', async (req, res) => {
+    const {name, email, pass} = req.body;
+    let userUpdate = await userModel.updateOne({name:name, email: email, pass: pass});
+    // let updateData = await userModel.find({name:name});
+    // res.redirect(`/profile/${updateData.name}`);
+});
+
+app.get('/delete', async (req, res) => {
+    res.render("delete");
+});
+
+// delete in userdata page 
+app.get('/delete/:useremail', async (req, res) => {
+    let deleteuser = await userModel.findOneAndDelete({email: req.params.useremail});
+    res.redirect("/userdata");
+});
+
+// delete in profile 
+app.get('/profile/:useremail/delete', async (req, res) => {
+    let deleteprofileuser = await userModel.findOneAndDelete({email: req.params.useremail});
+    res.redirect("/userdata");
+});
+
+// delete page user delete 
+app.post('/delete', async (req, res) => {
+    const { name, pass } = req.body;
+    try {
+        let deleteuser = await userModel.findOneAndDelete({ name: name, pass: pass });
+        if (deleteuser) {
+            res.render('delete', { message: `User ${name} has been successfully deleted.` });
+        } else {
+            res.render('delete', { message: `User ${name} not found or incorrect password.` });
+        }
+    } catch (err) {
+        console.error(err);
+        res.render('delete', { message: `Error deleting user` });
+    }
+});
+
 
 app.listen(3000, () => {
     console.log('Server is running on port http://localhost:3000/');
